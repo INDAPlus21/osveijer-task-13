4742a62baafdb4ca98baabad5275ac7c727f0499
Oliver Sveijer <osveijer@ug.kth.se>
2021-12-21 20:04:12 +0100

    Code for getting rays and setting background

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,51 @@ use std::path::Path;
 use std::fs::File;
 use std::io::{BufWriter};
 use png;
+use vecmath::{self, Vector3, vec3_scale, vec3_add, vec3_sub, vec3_normalized};
+
+#[derive(Copy,Clone)]
+struct Ray {
+    a: Vector3<f32>,
+    b: Vector3<f32>
+}
+
+impl Ray {
+    pub fn new(_a: Vector3<f32>, _b: Vector3<f32>) -> Ray {
+        Ray { a: _a, b: _b }
+    }
+
+    pub fn origin(self) -> Vector3<f32> {
+        self.a
+    }
+    
+    pub fn direction(self) -> Vector3<f32> {
+        self.b
+    }
+
+    pub fn point_by_t(self, t: f32) -> Vector3<f32> {
+        vec3_add(self.a, vec3_scale(self.b, t))
+    }
+}
+
+struct Camera {
+    c: Vector3<f32>,
+    tl: Vector3<f32>,
+    tr: Vector3<f32>,
+    bl: Vector3<f32>,
+    br: Vector3<f32>
+}
+
+impl Camera {
+    pub fn new(_c: Vector3<f32>) -> Camera {
+        Camera { 
+            c: _c, 
+            tl: vec3_add(_c, [-2.0,1.0,-1.0]), 
+            tr: vec3_add(_c, [2.0,1.0,-1.0]), 
+            bl: vec3_add(_c, [-2.0,-1.0,-1.0]), 
+            br: vec3_add(_c, [2.0,-1.0,-1.0]) 
+        }
+    }
+}
 
 fn main() {
     let path = Path::new(r"test.png");
@@ -9,7 +54,7 @@ fn main() {
     let ref mut w = BufWriter::new(file);
 
     let width:usize = 200;
-    let height: usize = 200;
+    let height: usize = 100;
 
     let mut encoder = png::Encoder::new(w, width as u32, height as u32); 
     encoder.set_color(png::ColorType::Rgba);
@@ -26,16 +71,42 @@ fn main() {
     encoder.set_source_chromaticities(source_chromaticities);
     let mut writer = encoder.write_header().unwrap();
 
-    let mut data: [u8; 200 * 200 * 4] = [0; 200 * 200 * 4];
-    for i in 0..200 {
-        for j in 0..200 {
-            assign_pixel(i*200+j, ((i as f32/200.0)*255.0) as u8, ((j as f32/200.0)*255.0) as u8, 0, 255, &mut data)
-        }
-    }
+    let mut data: [u8; 200 * 100 * 4] = [0; 200 * 100 * 4];
+    let rays = create_rays(Camera::new([0.0,0.0,0.0]), width, height);
+    set_background(rays, &mut data);
+    //for i in 0..200 {
+        //for j in 0..200 {
+            //assign_pixel(i*200+j, ((i as f32/200.0)*255.0) as u8, ((j as f32/200.0)*255.0) as u8, 0, 255, &mut data)
+        //}
+    //}
     writer.write_image_data(&data).unwrap();
 }
 
-fn assign_pixel(pixel: usize, r: u8, g: u8, b: u8, a: u8, data: &mut [u8; 200 * 200 * 4]) {
+fn create_rays(_camera: Camera, _w: usize, _h: usize) -> Vec<Ray>{
+    let mut rays: Vec<Ray> = Vec::new();
+
+    let top_left = vec3_sub(_camera.tl,_camera.c);
+    let width_offset = vec3_sub(_camera.tr,_camera.tl);
+    let height_offset = vec3_sub(_camera.bl,_camera.tl);
+
+    for i in 0.._h {
+        for j in 0.._w {
+            rays.push(Ray::new(_camera.c, vec3_add(top_left,vec3_add(vec3_scale(height_offset, i as f32 / _h as f32), vec3_scale(width_offset, j as f32 / _w as f32)))));
+        }
+    }
+
+    rays
+}
+
+fn set_background(rays: Vec<Ray>, data: &mut [u8; 200 * 100 * 4]) {
+    for i in 0..(rays.len() - 1) {
+        let t = 0.5 * (vec3_normalized(rays[i].direction())[1] + 1.0);
+        let c = vec3_add(vec3_scale([255.0,255.0,255.0], 1.0-t),vec3_scale([75.0,150.0,255.0], t));
+        assign_pixel(i, c[0] as u8, c[1] as u8, c[2] as u8, 255, data);
+    }
+}
+
+fn assign_pixel(pixel: usize, r: u8, g: u8, b: u8, a: u8, data: &mut [u8; 200 * 100 * 4]) {
     let i = pixel * 4;
     data[i] = r;
     data[i+1] = g;
